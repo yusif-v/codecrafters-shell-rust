bc68fab0c4ba3d538673626077d7d784b493b7dd
lizard <code.yusif.v@gmail.com>
2026-07-19 21:16:48 +0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, BufRead, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    loop {
+        // Display the prompt (from the previous stage)
+        print!("$ ");
+        stdout.flush().unwrap();
+
+        // Read the user's input
+        let mut input = String::new();
+        if stdin.lock().read_line(&mut input).unwrap() == 0 {
+            break; // EOF
+        }
+
+        let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
+
+        // Treat the first whitespace-separated token as the command.
+        // For now, every command is invalid.
+        let command = input.split_whitespace().next().unwrap();
+        println!("{}: command not found", command);
+    }
 }
